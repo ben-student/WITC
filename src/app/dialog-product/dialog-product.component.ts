29e78340c2b65b3a0823fbe87f1c975e4cea6be1
ben-student <s108652@ap.be>
2021-06-05 10:53:23 +0200

    changed some stuff

--- src/app/dialog-product/dialog-product.component.ts
+++ src/app/dialog-product/dialog-product.component.ts
@@ -11,19 +11,19 @@ export class DialogProductComponent implements OnInit {
   constructor(private dialogRef: MatDialogRef<DialogProductComponent>,private ms : MailServiceService) { }
   images = [];
   product:string;
-  price:string;
-  description:string;
-  STOEL:string;
+
+  
+  
 
   ngOnInit(): void {
 
     this.product = this.ms.getProduct();
-    for(let i = 0; i<8;i++){
+    for(let i = 0; i<25;i++){
       this.images[i] = `assets/${this.product}${i+1}.jpg`;
       console.log(this.images[i]);
     }
 
-    this.STOEL = "Chairs are pretty difficult to design and make as it needs to look good, be strong enough to bear the weight of a human being and withstand the abuse of the human using it. I tried it with this lounge chair and i am pretty happy with the result. Made out of 48 layers of black MDF and another 3 layers per. Now happily residing in my brothers apartement. As this piece needs to be strong enough I'm still testing it to see if it can stand the test of time. But it will be available soon! If you have any questions, feel free to mail me at Sam.dupont2001@gmail.com with #20192";
+    
 
 
     
